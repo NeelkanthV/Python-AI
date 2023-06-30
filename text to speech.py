847798f3edd3ace4c8c2da8fbe79be148de65ae4
Neelkanth <117567013+NeelkanthV@users.noreply.github.com>
2023-06-29 21:10:34 -1200

    Update text to speech.py

--- text to speech.py
+++ text to speech.py
@@ -3,6 +3,7 @@ from cgitb import text
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
+# For a male voice, type [0], and for a female voice, type[1]!
 engine.setProperty('voice',voices[0].id)
 text = "Somewhere,something incredible is waiting to be known!"
 engine.say(text)
